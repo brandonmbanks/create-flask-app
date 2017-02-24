502b42df3cf92aa516214ce864801498575d8c31
Brandon Banks <b93banks@gmail.com>
2017-02-24 17:28:01 -0500

    use config values in app and add config option to env.example

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from dotenv import load_dotenv, find_dotenv
 from routes import routes_blueprint
+import os
 
 load_dotenv(find_dotenv())
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 app.register_blueprint(routes_blueprint)
 
