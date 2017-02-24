2f765b292302f2929634be83a9afd46b5d8609ff
Brandon Banks <b93banks@gmail.com>
2017-02-24 08:24:09 -0500

    fix python dockerfile and use python3 to start wsgi

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.6
 ENV PYTHONUNBUFFERED 1
-ADD . /src/
-WORKDIR /src
+ADD . /var/www
+WORKDIR /var/www
 RUN pip3 install -r requirements.txt
--- deploy/start.sh
+++ deploy/start.sh
@@ -1,6 +1,6 @@
 if [ "$FLASK_ENV" == "development" ]
 then
-    python wsgi.py
+    python3 wsgi.py
 else
     /usr/local/bin/gunicorn wsgi -w 2 -b :5000 --worker-class gevent --reload;
 fi
