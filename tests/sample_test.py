3f933a5466e7538ee2470f414f68d565bc8ee915
Brandon Banks <b93banks@gmail.com>
2017-02-24 21:29:41 -0500

    add test for env variables

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -1,8 +1,13 @@
 import unittest
 from app import app
+import os
 
 class SampleTest(unittest.TestCase):
 
     def test_two_plus_two_equals_4(self):
         result = 2 + 2
         self.assertEqual(4, result)
+
+    def test_it_should_get_env_variables(self):
+        result = os.getenv('APP_ENV')
+        self.assertEqual('local', result)
