3997faf647735211ee0cfe390dcf8212df9d181d
Brandon Banks <b93banks@gmail.com>
2017-02-24 21:44:56 -0500

    remove sample test and rename file

--- tests/config_test.py
+++ tests/config_test.py
@@ -2,11 +2,7 @@ import unittest
 from app import app
 import os
 
-class SampleTest(unittest.TestCase):
-
-    def test_two_plus_two_equals_4(self):
-        result = 2 + 2
-        self.assertEqual(4, result)
+class ConfigTest(unittest.TestCase):
 
     def test_it_should_correctly_set_app_config(self):
         result = os.getenv('APP_SETTINGS')
